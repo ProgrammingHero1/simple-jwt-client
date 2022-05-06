496f8818d369d9359a8b1f9411134d6e71f29729
Programming Hero <programming.hero1@gmail.com>
2022-05-06 11:15:09 -0500

    send jwt token and verify it

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Orders = () => {
+    const navigate = useNavigate();
+    const [orders, setOrders] = useState([]);
+
+    useEffect( () =>{
+        fetch('http://localhost:5000/orders', {
+            method: 'GET', 
+            headers:{
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => {
+            if(res.status === 401 || res.status===403){
+                navigate('/login');
+            }
+            return res.json()
+        })
+        .then(data =>{
+            console.log(data);
+            setOrders(data);
+        })
+    }, [])
+
     return (
         <div>
-            <h1>This is Orders page</h1>
+            <h1>This is Orders page: {orders.length}</h1>
         </div>
     );
 };
